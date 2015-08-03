3d734b993aba2499cdc97df2a67af2756a86ca4d
Vasilyeva Olga <olga.vasilyeva@levelupers.com>
2015-08-03 10:03:47 +0300

    Add two templates

--- app/code/local/Cat/Expensive/Block/Topsearches.php
+++ app/code/local/Cat/Expensive/Block/Topsearches.php
@@ -3,8 +3,9 @@ class Cat_Expensive_Block_Topsearches
     extends Mage_Core_Block_Template
     implements Mage_Widget_Block_Interface
 {
-    protected function _toHtml()
+    public function getExpensiveProducts()
     {
+
         $products_count = $this->getData("products_count");
         $productCollection = Mage::getModel('catalog/product')
             ->getCollection()
@@ -13,15 +14,7 @@ class Cat_Expensive_Block_Topsearches
             ->addAttributeToSelect('*')
             ->setOrder('price', 'desc');
         $productCollection->getSelect()->limit($products_count);
-        $html = '<div class="widget-expensive-block">' ;
-        $html .= '<p class="widget-expensive-title">The Most Expensive Products</p>' ;
-
-        foreach($productCollection as $product){
-            $html .=  "<a href='".$product->getProductUrl()."'>".$product->getName()."</a></br>";
-        }
-        $html .= '</div>';
-        return $html;
-
+        return $productCollection;
     }
 
 };
